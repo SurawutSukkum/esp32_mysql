ced7edccac25b0295221343a8b21c75187d35366
SurawutSukkum <74821839+SurawutSukkum@users.noreply.github.com>
2025-05-12 10:48:43 +0700

    Update ServerMesh.ino

--- Mesh/ServerMesh.ino
+++ Mesh/ServerMesh.ino
@@ -1,6 +1,6 @@
-#include <painlessMesh.h>
 #include <WiFi.h>
-#include <HTTPClient.h>  // Only on the server node
+#include <HTTPClient.h>
+#include <painlessMesh.h>
 
 #define   MESH_PREFIX     "yourMeshNetwork"
 #define   MESH_PASSWORD   "meshPassword"
@@ -11,25 +11,27 @@ painlessMesh  mesh;
 
 const char* ssid = "YourWiFiSSID";
 const char* password = "YourWiFiPassword";
-const char* serverUrl = "http://yourapi.com/data";
+const char* serverUrl = "http://yourserver.com/insert.php";
 
-void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("Received from %u: %s\n", from, msg.c_str());
-  sendToAPI(msg);  // Send to cloud
-}
-
-void sendToAPI(const String& payload) {
+void sendToMySQL(uint32_t fromNode, const String& msg) {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     http.begin(serverUrl);
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST("{\"data\":\"" + payload + "\"}");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    Serial.printf("HTTP Response code: %d\n", httpResponseCode);
+    String postData = "node=" + String(fromNode) + "&data=" + msg;
+
+    int httpResponseCode = http.POST(postData);
+    Serial.printf("[HTTP] code: %d\n", httpResponseCode);
     http.end();
   } else {
     Serial.println("WiFi not connected");
   }
+}
+
+void receivedCallback(uint32_t from, String &msg) {
+  Serial.printf("Received from %u: %s\n", from, msg.c_str());
+  sendToMySQL(from, msg);
 }
 
 void connectWiFi() {
@@ -38,13 +40,12 @@ void connectWiFi() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Connected to WiFi");
+  Serial.println("WiFi connected");
 }
 
 void setup() {
   Serial.begin(115200);
   connectWiFi();
-
   mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
   mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
