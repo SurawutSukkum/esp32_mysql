4999ecabd396d32afa6902305601604f836cf59a
SurawutSukkum <74821839+SurawutSukkum@users.noreply.github.com>
2025-05-16 21:01:53 +0700

    Update and rename insert.php to insert1.php

--- Mesh/insert1.php
+++ Mesh/insert1.php
@@ -8,10 +8,12 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $val = $_GET['node'];
-$val1 = $_GET['data'];
-$sql = "INSERT INTO mesh_data(node,data) VALUES ($val,$val1);";
+$val1 = $_GET['Temp'];
+$val2 = $_GET['Humi'];
+$val3 = $_GET['Pressure'];
+$val4 = $_GET['Led'];
+$sql = "INSERT INTO mesh_dataV1 (node,Temp,Humi,Pressure,Led) VALUES ($val,$val1,$val2,$val3,$val4);";
 
 if ($conn->query($sql) === TRUE) {
     echo "save OK2";
